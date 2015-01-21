d19e23f5f18657b5e6bb6117b804168960ebc430
root <root@ubuntu.(none)>
2015-01-21 14:23:05 +0300

    views | routes | html5 mode

--- www_/app/js/app.js
+++ www_/app/js/app.js
@@ -1,5 +1,18 @@
 'use strict';
 
 angular.module('myApp', [
-    'myApp.controllers'
+    'myApp.controllers',
+    'ngRoute'
 ]);
+
+angular.module('myApp').config(function($routeProvider, $locationProvider){
+    $routeProvider.when('/view1', {
+	controller: 'Controller1',
+	templateUrl: 'partials/view1.html'
+    }).when('/view2', {
+	controller: 'Controller2',
+	templateUrl: 'partials/view2.html'
+    });
+    
+    $locationProvider.html5Mode(true); // activate HTML5 code
+});
--- www_/app/js/controllers.js
+++ www_/app/js/controllers.js
@@ -2,48 +2,8 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []);
-
-angular.module('myApp.controllers').controller('MessageController' , function($scope, $timeout){
-    $scope.messages = [{
-            sender: 'user1',
-            text: 'Message1'
-    }];
-    
-    var timer;
-    var count = 0;
-    $scope.loadMessages = function(){
-        count++;
-        $scope.messages.push({
-            sender : 'user1',
-            text : 'Random message' + count
-        });
-        timer = $timeout($scope.loadMessages, 2000);
-        
-        if(count == 3){
-            $scope.$broadcast('EVENT_NO_DATA', 'Not Connected');
-            $timeout.cancel(timer);
-        }
-    };
-    
-    timer = $timeout($scope.loadMessages, 2000);
-    
-    $scope.$on('EVENT_RECEIVED', function(){
-        console.log('Received emitted event');
-    });
-
-});
-
-angular.module('myApp.controllers').controller('StatsController', function($scope){
-    $scope.name = 'World';
-    $scope.status = 'Connected';
-    $scope.statusColor = 'green';
-    
-    $scope.$on('EVENT_NO_DATA', function(event, data){
-        console.log('received broadcasted event');
-        $scope.status = data;
-        $scope.statusColor = 'Red';
-        
-        $scope.$emit('EVENT_RECEIVED');
-    });
+angular.module('myApp.controllers', []).controller('Controller1', function($scope){
+    $scope.message = "Hello world";
+}).controller('Controller2', function($scope){
+    $scope.now = new Date();
 });
