a5ec0084ac5649e74654658f6b5d04d88f51f91d
root <root@ubuntu.(none)>
2015-02-04 16:46:16 +0300

    first blog commit

--- Ch 02/sp-blogger/app/js/app.js
+++ Ch 02/sp-blogger/app/js/app.js
@@ -3,3 +3,7 @@
 angular.module('spBlogger',['spBlogger.posts','spBlogger.controllers','spBlogger.directives','spBlogger.filters','spBlogger.services']);
 
 angular.module('spBlogger').value('version','V1.0');
+
+angular.module('spBlogger').run(['$state', function($state){
+    $state.go('allPosts');
+}]);
--- Ch 02/sp-blogger/app/modules/posts/postModule.js
+++ Ch 02/sp-blogger/app/modules/posts/postModule.js
@@ -1,3 +1,17 @@
 'use strict'
 
 angular.module('spBlogger.posts',['spBlogger.posts.controllers','spBlogger.posts.directives','spBlogger.posts.services','spBlogger.posts.filters']);
+
+angular.module('spBlogger.posts').config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider){
+    $stateProvider.state('allPosts', {
+	url: '/posts',
+	controller : 'PostController',
+	templateUrl : 'modules/posts/views/posts.html'
+    });
+    
+    $stateProvider.state('', {
+	url : '/posts/:id/:permalink',
+	controller : 'PostDetailsController',
+	templateUrl : 'modules/posts/views/singlePost.html'
+    });
+}]);
