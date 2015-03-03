0fea4ca8050b6f4408e91582b62af1aa52559e86
valery-kuzmenko <valerakyz@list.ru>
2015-03-03 12:59:41 -0800

    REST api

--- Ch 02/sp-blogger/app/js/app.js
+++ Ch 02/sp-blogger/app/js/app.js
@@ -1,6 +1,7 @@
 'use strict'
 
-angular.module('spBlogger',['ui.router', 'spBlogger.posts', 'spBlogger.controllers','spBlogger.directives','spBlogger.filters','spBlogger.services', 'spBlogger.admin',]);
+angular.module('spBlogger',['ui.router', 'spBlogger.posts', 'spBlogger.controllers','spBlogger.directives',
+                            'spBlogger.filters','spBlogger.services', 'spBlogger.admin', 'ngResource']);
 
 angular.module('spBlogger').value('version','V1.0');
 
--- Ch 02/sp-blogger/app/modules/admin/js/services.js
+++ Ch 02/sp-blogger/app/modules/admin/js/services.js
@@ -1,5 +1,14 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+'use strict'
+
+angular.module('spBlogger.admin.services', []).factory('Post', ['$resource', 'API_ENDPOINT', function($resource){
+    return $resource(API_ENDPOINT, {id : '@_id'} , {
+        update : {
+            method: 'PUT'
+        }
+    });
+    
+}]).service('popupService', ['$window', function($window){
+    this.showPopup = function(){
+        return $window.confirm(message);
+    };
+}]).value('API_ENDPOINT', 'http://blog.loc/api/posts/:id');
